7d249dbb7de1bf1f6293d28541e43149c2295ac4
carlzhang4 <2470871694@qq.com>
2019-03-20 02:09:05 +0800

    restart

---
+++ src/main/Tool.java
@@ -0,0 +1,16 @@
+package main;
+public class Tool{
+	public static void print(String content){
+		System.out.println(content);
+	}
+	public static void print(int content){
+		System.out.println(content);
+	}
+	public static void print(double content){
+		System.out.println(content);
+	}
+	public static void print(float content){
+		System.out.println(content);
+		
+	}
+}
